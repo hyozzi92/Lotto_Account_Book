3ecf5ef97681437a6f749e798c896725f59907f3
hyozzi92 <gytjdzja@gmail.com>
2022-12-04 17:43:03 +0900

    배포완료

--- src/Main/MapContainer.jsx
+++ src/Main/MapContainer.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './MapContainer.scss';
 //안될 시 https://kjwan4435.tistory.com/66 참고할 것
-const { kakao } = window;
 
 const MapContainer = ({ searchPlace }) => {
+  const { kakao } = window;
   // 검색결과 배열에 담아줌
   const [Places, setPlaces] = useState([]);
 
   useEffect(() => {
-    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-    var mapContainer = document.getElementById('myMap'), // 지도를 표시할 div
+    let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    let mapContainer = document.getElementById('map'), // 지도를 표시할 div
       mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 10, // 지도의 확대 레벨
       };
 
-    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+    let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
     // HTML5의 geolocation으로 사용할 수 있는지 확인합니다
     if (navigator.geolocation) {
       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
       navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = position.coords.latitude, // 위도
+        let lat = position.coords.latitude, // 위도
           lon = position.coords.longitude; // 경도
 
-        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+        let locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
           message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
 
         // 마커와 인포윈도우를 표시합니다
@@ -33,7 +33,7 @@ const MapContainer = ({ searchPlace }) => {
     } else {
       // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
 
-      var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+      let locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
         message = 'geolocation을 사용할수 없어요..';
 
       displayMarker(locPosition, message);
@@ -42,16 +42,16 @@ const MapContainer = ({ searchPlace }) => {
     // 지도에 마커와 인포윈도우를 표시하는 함수입니다
     function displayMarker(locPosition, message) {
       // 마커를 생성합니다
-      var marker = new kakao.maps.Marker({
+      let marker = new kakao.maps.Marker({
         map: map,
         position: locPosition,
       });
 
-      var iwContent = message, // 인포윈도우에 표시할 내용
+      let iwContent = message, // 인포윈도우에 표시할 내용
         iwRemoveable = true;
 
       // 인포윈도우를 생성합니다
-      var infowindow = new kakao.maps.InfoWindow({
+      let infowindow = new kakao.maps.InfoWindow({
         content: iwContent,
         removable: iwRemoveable,
       });
@@ -87,7 +87,7 @@ const MapContainer = ({ searchPlace }) => {
 
     // 검색결과 목록 하단에 페이지 번호 표시
     function displayPagination(pagination) {
-      var paginationEl = document.getElementById('pagination'),
+      let paginationEl = document.getElementById('pagination'),
         fragment = document.createDocumentFragment(),
         i;
 
@@ -97,7 +97,7 @@ const MapContainer = ({ searchPlace }) => {
       }
 
       for (i = 1; i <= pagination.last; i++) {
-        var el = document.createElement('a');
+        let el = document.createElement('a');
         el.href = '#';
         el.innerHTML = i;
 
@@ -137,14 +137,14 @@ const MapContainer = ({ searchPlace }) => {
     <div>
       <div className="mapWrap">
         <div
-          id="myMap"
+          id="map"
           style={{
             width: '500px',
             height: '500px',
           }}
         ></div>
         <div className="resultList">
-          {Places.map((item, i) => (
+          {Places?.map((item, i) => (
             <div key={i} style={{ marginTop: '20px' }}>
               <span>{i + 1}</span>
               <div>
